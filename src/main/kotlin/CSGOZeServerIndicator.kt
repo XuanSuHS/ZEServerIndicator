a44c68b4d52b088265d19ece658cb1253789ce4d
XuanSuHS <XuanSuHSia@outlook.com>
2023-04-30 18:10:08 +0800

    Bump Version to v0.1.9-B6

--- src/main/kotlin/CSGOZeServerIndicator.kt
+++ src/main/kotlin/CSGOZeServerIndicator.kt
@@ -19,7 +19,7 @@ object Indicator: KotlinPlugin(
     JvmPluginDescription(
         id = "top.xuansu.mirai.ze-server-indicator",
         name = "CSGO Ze Server Indicator",
-        version = "0.1.9-B5",
+        version = "0.1.9-B6",
     ) {
         author("XuanSu")
     }
