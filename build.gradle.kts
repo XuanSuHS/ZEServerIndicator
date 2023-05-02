fc846c34abcdd5506a66dd4d0c3515ccd53c0980
XuanSu <XuanSuHSia@outlook.com>
2023-05-02 15:20:34 +0800

    feat: 序列化topze服务器数据

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.xuansu.mirai.zeServerIndicator"
-version = "0.1.9-B18"
+version = "0.1.10-B1"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/kotlin/Coroutine.kt
+++ src/main/kotlin/Coroutine.kt
@@ -1,26 +1,21 @@
 package top.xuansu.mirai.zeServerIndicator
 
 import kotlinx.coroutines.*
+import net.mamoe.mirai.utils.info
+import top.xuansu.mirai.zeServerIndicator.Indicator.logger
 
 
 fun ubAsync() {
-    CoroutineScope(Dispatchers.IO).launch {UB.webForUB()}
+    CoroutineScope(Dispatchers.IO).launch { UB.webForUB() }
     while (true) {
         while (UB.isWebsocketFailed) {
             Thread.sleep(7000)
             UB.isWebsocketFailed = false
-            CoroutineScope(Dispatchers.IO).launch {UB.webForUB()}
+            CoroutineScope(Dispatchers.IO).launch { UB.webForUB() }
         }
         Thread.sleep(2500)
     }
 }
-
-fun zedAsync() {
-    CoroutineScope(Dispatchers.IO).launch {
-        Zed.webForZED()
-    }
-}
-
 
 fun coroutineOnEnable() {
 
@@ -28,11 +23,31 @@ fun coroutineOnEnable() {
     CoroutineScope(Dispatchers.IO).launch { ubAsync() }
     CoroutineScope(Dispatchers.IO).launch {
         Zed.webForZED()
+        logger.info { "ZED数据已初始化" }
         while (true) {
             withContext(Dispatchers.IO) {
                 Thread.sleep(15000)
             }
-            zedAsync()
+            CoroutineScope(Dispatchers.IO).launch {
+                Zed.webForZED()
+            }
+        }
+    }
+    CoroutineScope(Dispatchers.IO).launch {
+        CoroutineScope(Dispatchers.IO).launch {
+            TopZE.initializeMapData()
+            TopZE.webForTopZE()
+            logger.info { "5e地图数据已加载" }
+        }
+        TopZE.webForTopZE()
+        logger.info { "5e数据已初始化" }
+        while (true) {
+            withContext(Dispatchers.IO) {
+                Thread.sleep(15000)
+            }
+            CoroutineScope(Dispatchers.IO).launch {
+                TopZE.webForTopZE()
+            }
         }
     }
     var time = 0
